b42d63712816e91f26a4e0cd5b476a6e8b44dab1
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-10-04 14:29:21 +0200

    feat: add nav bar

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,42 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 import GET_CURRENTUSER from "../queries/CurrentUser";
 
 const Header = () => {
     const { loading, error, data } = useQuery(GET_CURRENTUSER);
-    if (loading) return null;
-    console.log(data);
+
+    if (loading) return <div/>;
+
+    const renderButtons = () => {
+    if (data.currentUser) {
+            return <div>Logout</div>;
+        } else {
+            return (
+            <div>
+                <li>
+                    <Link to="/signup">Signup</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+            </div>
+            );
+        }
+    }
 
     return (
-        <div>
-            <h1>My Header</h1>
-        </div>
+        <nav>
+            <div className="nav-wrapper">
+                <Link to="/" className="brand-logo left">
+                    Home
+                </Link>
+                <ul className="right">
+                    {renderButtons()}
+                </ul>
+            
+            </div>
+        </nav>
     )
 }
 
