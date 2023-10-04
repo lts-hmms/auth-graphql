6d7f8a663a0618ee704e3c11c452656e43eb4efd
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-10-04 11:36:36 +0200

    feat: add apollo client

--- client/index.js
+++ client/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-const Root = () => {
-  return (
-    <div>
-      Auth Starter
-    </div>
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <ApolloProvider client={client}>
+      <div>
+        Auth Starter
+      </div>
+    </ApolloProvider>
   );
-};
-
-ReactDOM.render(<Root />, document.querySelector('#root'));
