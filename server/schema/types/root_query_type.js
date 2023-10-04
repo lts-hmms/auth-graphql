85ffadbdb288a02411e9b218a2783e547cbaeac6
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-10-04 14:28:58 +0200

    feat: add materialize css

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -5,6 +5,7 @@ const UserType = require('./user_type');
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    // This is the query that will be used to check if a user is authenticated. If not, user will be null.
     currentUser: {
       type: UserType,
       resolve(parentValue, args, req) {
