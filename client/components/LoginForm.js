42573c55ff63f80ca9164fafaafca5da26f7cce5
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-10-06 13:20:34 +0200

    feat: importing login mutation and submit func

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,11 +1,22 @@
 import React from "react";
 import AuthForm from "./AuthForm";
+import { useMutation } from "@apollo/client";
+import LoginMutation from "../mutations/LoginMutation";
 
 const LoginForm = () => {
+    const [login] = useMutation(LoginMutation);
+
+    
+    const onSubmit = ({ email, password }) => {
+        login({
+            variables: { email, password }
+        });
+    }
+
     return (
         <div>
             <h3>Login</h3>
-            <AuthForm />
+            <AuthForm onSubmit={onSubmit} />
         </div>
     )
 }
