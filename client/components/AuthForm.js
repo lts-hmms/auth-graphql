c9196d668225a3bdd30dccadceb4221bc391e36e
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-10-25 19:01:10 +0200

    feat: aria label and focus email
    - aria-Label for screenreaders added to form
    - after rendering email form field is focused

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,26 +1,33 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const AuthForm = ({ onSubmit, errors }) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const emailRef = useRef(null);
 
     const onFormSubmit = (e) => {
         e.preventDefault();
         onSubmit({ email, password });
     }
 
+    // render the email input field with focus
+    useEffect(() => {
+        emailRef.current.focus();
+    }, []);
+
     return (
-        // ensure that app has class container
+        // ensure that App.js component has class 'container' to center the form
         <div className="row">
             <form onSubmit={onFormSubmit} className="col s6">
                 <div className="input-field">
                     <input 
+                        ref={emailRef}
                         placeholder="Email"
                         value={email}
                         onChange={e => setEmail(e.target.value)}
+                        aria-label="Email"
                     />
                 </div>
                 <div className="input-field">
@@ -29,6 +36,7 @@ const AuthForm = ({ onSubmit, errors }) => {
                         type="password"
                         value={password}
                         onChange={e => setPassword(e.target.value)}
+                        aria-label="Password"
                     />
                 </div>
                 <div className="errors">
